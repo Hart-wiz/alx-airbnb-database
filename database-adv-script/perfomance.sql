43a076ee822c1c9b198f164c59db6c704943ae4a
hart-wiz <73368468+Hart-wiz@users.noreply.github.com>
2025-05-18 13:43:50 +0100

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,17 @@
+-- Initial complex query to retrieve full booking details
+SELECT 
+    b.booking_id,
+    b.date,
+    u.user_id,
+    u.name AS user_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+JOIN payments pay ON b.payment_id = pay.payment_id;
