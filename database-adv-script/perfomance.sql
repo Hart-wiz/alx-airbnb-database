967fb308f5c9bb4e8c747c4e4291bf24f45cb117
hart-wiz <73368468+Hart-wiz@users.noreply.github.com>
2025-05-18 13:45:33 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -15,3 +15,20 @@ FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 JOIN payments pay ON b.payment_id = pay.payment_id;
+
+
+ -- analyze query performance 
+
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.date,
+    u.user_id,
+    u.name,
+    p.name,
+    pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+JOIN payments pay ON b.payment_id = pay.payment_id;
+
