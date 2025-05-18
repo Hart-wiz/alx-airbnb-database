501d0f45bfafd7f31d7f2dde80902f809e379d54
hart-wiz <73368468+Hart-wiz@users.noreply.github.com>
2025-05-18 13:57:13 +0100

    Update perfomance.sql added where & and

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,25 @@
+
 -- Initial complex query to retrieve full booking details
+SELECT 
+    b.booking_id,
+    b.date,
+    u.user_id,
+    u.name AS user_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+JOIN payments pay ON b.payment_id = pay.payment_id;
+
+-- Initial complex query to retrieve detail with filter(WHERE, AND)
+
+SELECT
 b.booking_id,
     b.date,
     u.name AS user_name,
